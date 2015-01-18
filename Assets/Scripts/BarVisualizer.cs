23236fbacce472b1fb5aea99a3cc03862b5ccdfa
Mohammad Doleh <mdoleh@kent.edu>
2015-01-18 01:18:22 -0500

    Bar visualizer

---
+++ Assets/Scripts/BarVisualizer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BarVisualizer : Visualizer
+{
+
+    private Vector3 startScale;
+    public float minValue = 1.0f;
+    public float amplitude = 0.1f;
+    private float randomAmplitude;
+
+    private void Start()
+    {
+        startScale = transform.localScale;
+    }
+
+    public override void ChangeVisual(float dbValue, float pitchValue)
+    {
+        randomAmplitude = Random.Range(0.5f, 1.5f);
+        transform.localScale = new Vector3((dbValue * amplitude * randomAmplitude) + startScale.x, minValue, minValue);
+    }
+}
